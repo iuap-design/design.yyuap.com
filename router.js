7554034daef0a06b6d16b927e0d8862ec71476ab
yaoxinc <yaoxinc@yonyou.com>
2016-10-26 17:54:16 +0800

    加入商务合作信息提交的方法

--- router.js
+++ router.js
@@ -2,12 +2,18 @@
  * Created by chief on 16/10/18.
  */
 
-/**
- * Created by chief on 16/9/21.
- */
-
-
 var router = require('koa-router')();
+/**
+ * 2016.10.26 businessCooperation userform submit
+ * */
+
+var parse = require('co-busboy');
+var koaBody = require('koa-body')();
+var fs = require('fs');
+var os = require('os');
+var path = require('path');
+var unzip  = require('unzip');
+
 
 //上传
 router.get('/upload', function*(next) {
@@ -22,4 +28,17 @@ router.get('/businessCooperation', function*(next) {
 router.get('/case', function*(next) {
     yield this.render('case');
 });
+
+/**
+ * 2016.10.26 businessCooperation userform submit
+ * */
+
+router.post('/form/add',koaBody,function *(){
+    var model = require('./models/product');
+
+    var data = yield model.addUserForm(this.request.body);
+
+    this.body=data;
+});
+
 exports.router = router;
