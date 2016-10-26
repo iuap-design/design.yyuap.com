352376d0c48f58a953c57fd1d236bb7dc791c837
yaoxinc <yaoxinc@yonyou.com>
2016-10-26 17:45:11 +0800

    创建数据库链接信息

---
+++ config/database.js
@@ -0,0 +1,13 @@
+/**
+ * Created by chief on 16/10/26.
+ */
+
+module.exports = {
+    mysql: {
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database:'design',
+        port: 3306
+    }
+}
